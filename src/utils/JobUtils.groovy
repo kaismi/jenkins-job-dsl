11d744b3c7679781ef3d52ef3fb1af507e9ac11b
Michael Kaiser <michael.kaiser@infonova.com>
2018-02-10 14:54:45 +0100

    Initial

--- src/utils/JobUtils.groovy
+++ src/utils/JobUtils.groovy
@@ -2,15 +2,16 @@ package utils
 
 class JobUtils {
 
-    static def createBuildJob(project, dslFactory) {
-        def branchApi = new URL("https://api.github.com/repos/kaismi/${project}/branches")
+    static
+    def createBuildJob(project, dslFactory, branchApiPrefix = "https://api.github.com/repos/kaismi/", gitUrlPrefix = "git://github.com/kaismi/") {
+        def branchApi = new URL(branchApiPrefix + "${project}/branches")
         def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
         branches.each {
             def branchName = it.name
             def jobName = "${project}-${branchName}".replaceAll('/', '-')
             dslFactory.job(jobName) {
                 scm {
-                    git("git://github.com/kaismi/${project}.git", branchName)
+                    git(gitUrlPrefix + "${project}.git", branchName)
                 }
                 steps {
                     maven("clean package")
