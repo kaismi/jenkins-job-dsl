df47842944bad9ef84ec1655e186d47f0f3a3f87
Michael Kaiser <michael.kaiser@infonova.com>
2018-02-10 13:02:14 +0100

    Initial

---
+++ src/jobs/task-assigner.groovy
@@ -0,0 +1,17 @@
+package jobs
+
+def project = 'task-assigner'
+def branchApi = new URL("https://api.github.com/repos/kaismi/${project}/branches")
+def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+branches.each {
+    def branchName = it.name
+    def jobName = "${project}-${branchName}".replaceAll('/','-')
+    job(jobName) {
+        scm {
+            git("git://github.com/kaismi/${project}.git", branchName)
+        }
+        steps {
+            maven("clean package")
+        }
+    }
+}
